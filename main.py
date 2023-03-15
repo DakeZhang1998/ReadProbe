62d45b3dcefd43fbc04f43ebea39950e6eda36a2
Dake Zhang <zhangdake1998@gmail.com>
2023-03-14 21:07:57 -0400

    Implemented the search function and established the dataflow.

--- main.py
+++ main.py
@@ -12,6 +12,13 @@ if st.button('Probe'):
     if input_text == 'Please copy and paste the text you want to probe into.':
         st.warning('Please input your text.')
     else:
-        st.write('Output from ChatGPT ...')
-
-
+        with st.spinner('Generating questions ...'):
+            questions = modules.generate_questions(input_text)
+        with st.spinner('Searching online ...'):
+            search_result = modules.bing_search(questions, top_n=3)
+        with st.spinner('Generating answers ...'):
+            for i, question in enumerate(questions):
+                st.markdown(f'{i + 1}. {question}')
+                st.markdown(modules.summarize(question, ' '.join([doc for url, doc in search_result[i]])))
+                for j in range(len(search_result[0])):
+                    st.markdown(f'\t{j + 1}. {search_result[i][j][0]}')
--- modules.py
+++ modules.py
@@ -1,1 +1,47 @@
 import openai
+import requests
+import streamlit as st
+from typing import List
+from bs4 import BeautifulSoup
+
+
+def bing_search(queries: List[str], top_n=3):
+    # This function takes as input a list of queries and
+    # return a list object containing Bing search results (top n) for each query.
+    headers = {'Ocp-Apim-Subscription-Key': st.secrets.azure_bing_keys.key}
+    return_list = []
+    for query in queries:
+        response = requests.get('https://api.bing.microsoft.com/v7.0/search', headers=headers,
+                                params={'q': query, 'textDecorations': True, 'textFormat': "HTML"})
+        response.raise_for_status()
+        search_results = response.json()
+        results_per_query = []
+        for result in search_results['webPages']['value'][:top_n]:
+            result_response = requests.get(result['url'])
+            soup = BeautifulSoup(result_response.content, 'html.parser')
+            text = soup.find('body').get_text()
+            clean_text = ''
+            for line in text.split('\n'):
+                clean_line = line.strip()
+                if len(clean_line.split(' ')) > 3:
+                    clean_text += f'{clean_line}\n'
+            results_per_query.append([result['url'], clean_text])
+        return_list.append(results_per_query)
+    return return_list
+
+
+def generate_questions(input_text: str):
+    # This function calls OpenAI APIs to generate a list of questions
+    # based on the input text given by the user.
+    return ['What is ReadProbe?', 'How should I use ReadProbe?', 'How does ReadProbe work?']
+
+
+def summarize(question: str, document: str):
+    # This function takes as input a pair of question and document to produce
+    # a short summary to answer the question using the information in the document.
+    return 'Summary produced by ChatGPT.'
+
+
+if __name__ == '__main__':
+    # This part is used for testing functions in this file.
+    results = bing_search(['hello?'])
