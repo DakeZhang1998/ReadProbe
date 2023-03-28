7f79e396d63ead76e76f37a0815fe16d6ee678ed
Dake Zhang <zhangdake1998@gmail.com>
2023-03-28 14:00:34 -0400

    Issues with Azure-provided ChatGPT. Fall back to OpenAI-provided ChatGPT for now.

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ top_n = 3  # Top n search results from Bing will be used to generate answers.
 n_questions = 5  # The number of questions that ChatGPT needs to come up with.
 enable_logging = True  # Turn on or off whether to log user feedback to Google Forms.
 st.set_page_config(layout='wide')
-chatgpt_provider = 'azure'  # openai or azure
+chatgpt_provider = 'openai'  # openai or azure
+
 
 if chatgpt_provider == 'openai':
     openai.api_key = st.secrets.openai_keys.key
